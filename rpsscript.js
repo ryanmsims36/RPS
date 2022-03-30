5c7790aa60bfff040a01e2993d91a784d42de133
Ryan <ryanmsims36@gmail.com>
2022-03-29 21:50:20 -0600

    Updating html and js files

--- rpsscript.js
+++ rpsscript.js
@@ -1,3 +1,6 @@
+let ps = 0;
+let cs = 0; 
+
 function computerPlay() {
 let possiblechoices = ['rock', 'paper', 'scissors']
 return possiblechoices[Math.floor(Math.random() * possiblechoices.length)];
@@ -5,17 +8,26 @@ return possiblechoices[Math.floor(Math.random() * possiblechoices.length)];
 
 
 function playRound(playerchoicelns,computerSelection) {
+
+
     if (playerchoicelns == 'rock'){
         switch(computerSelection) {
             case 'rock':
-                    return "It's a tie! Both chose rock!";
+                    document.getElementById("ann").textContent = "It's a tie! Both chose rock!";
                     break;
+                    
             case 'paper':
-                    return "You lost! Paper beats rock!";
+                    cs++;
+                    document.getElementById("CS").textContent = ["Computer Score: " + cs.toString()];
+                    document.getElementById("ann").textContent = "You lost! Paper beats rock!";
                     break;
+                    
             case 'scissors':
-                    return "You win! Rock beats scissors!";
+                    ps++;
+                    document.getElementById("PS").textContent = ["Player Score: " + ps.toString()];
+                    document.getElementById("ann").textContent = "You win! Rock beats scissors!";
                     break;
+                    
 
         }
     }
@@ -23,14 +35,21 @@ function playRound(playerchoicelns,computerSelection) {
     if (playerchoicelns == 'paper'){
         switch(computerSelection) {
             case 'paper':
-                    return "It's a tie! Both chose paper!";
+                    document.getElementById("ann").textContent = "It's a tie! Both chose paper!";
                     break;
+                    
             case 'scissors':
-                    return "You lost! Scissors beats paper!";
+                cs++;
+                document.getElementById("CS").textContent = ["Computer Score: " + cs.toString()];
+                    document.getElementById("ann").textContent = "You lost! Scissors beats paper!";
                     break;
+                    
             case 'rock':
-                    return "You win! Paper beats rock!";
+                ps++;
+                document.getElementById("PS").textContent = ["Player Score: " + ps.toString()];
+                    document.getElementById("ann").textContent =  "You win! Paper beats rock!";
                     break;
+                    
 
         }
     }
@@ -38,30 +57,71 @@ function playRound(playerchoicelns,computerSelection) {
     if (playerchoicelns == 'scissors'){
         switch(computerSelection) {
             case 'scissors':
-                    return "It's a tie! Both chose scissors!";
+                    document.getElementById("ann").textContent =  "It's a tie! Both chose scissors!";
                     break;
+                    
             case 'rock':
-                    return "You lost! Rock beats scissors!";
+                    cs++;
+                    document.getElementById("CS").textContent = ["Computer Score: " + cs.toString()];
+                    document.getElementById("ann").textContent = "You lost! Rock beats scissors!";
                     break;
+                    
             case 'paper':
-                    return "You win! Scissors beats paper!";
-                    break;
+                ps++;
+                document.getElementById("PS").textContent = ["Player Score: " + ps.toString()];
+                document.getElementById("ann").textContent =  "You win! Scissors beats paper!";
+                break;
+                    
 
         }
     }
+
+if (ps == 5)  {
+        ps = 0;
+        cs = 0;
+        document.getElementById("PS").textContent = ["Player Score: " + ps.toString()];
+        document.getElementById("CS").textContent = ["Computer Score: " + cs.toString()];
+        return document.getElementById("ann").textContent = "You Won the Game!";
+}
+if (cs == 5) {
+        ps = 0;
+        cs = 0;
+        document.getElementById("PS").textContent = ["Player Score: " + ps.toString()];
+        document.getElementById("CS").textContent = ["Computer Score: " + cs.toString()];
+        return document.getElementById("ann").textContent = "You Lost the Game!";
+}
+else {
+        return;
+}
 }
 
 function game() {
 
-    for(let i = 0; i < 5; i++){
-        let playerchoice = prompt("Enter either Rock, Paper, or Scissors");
-        playerchoicel = playerchoice.toLowerCase();
-        playerchoicelns = playerchoicel.replace(/\s+/g, '');
-        const computerSelection = computerPlay();
-        console.log(playRound(playerchoicelns,computerSelection));
-    }
+    //for(let i = 0; i < 5; i++){
+    //    let playerchoice = prompt("Enter either Rock, Paper, or Scissors");
+    //    playerchoicel = playerchoice.toLowerCase();
+    //    playerchoicelns = playerchoicel.replace(/\s+/g, '');
+    //    const computerSelection = computerPlay();
+     //   console.log(playRound(playerchoicelns,computerSelection));
+    //}
 
 
 }
+
+const btnr = document.querySelector('#btnr');
+btnr.addEventListener('click', () => {
+playRound("rock",computerPlay());
+});
+
+const btnp = document.querySelector('#btnp');
+btnp.addEventListener('click', () => {
+playRound("paper",computerPlay());
+});
+
+const btns = document.querySelector('#btns');
+btns.addEventListener('click', () => {
+playRound("scissors",computerPlay());
+});
+
 
 game()
